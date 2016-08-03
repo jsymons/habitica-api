0421ac1baf961e3d1da32bfed28dc9a155040615
Jason Symons <jason.symons@gmail.com>
2016-08-03 12:33:22 -0400

    Basic habit handling

--- user.py
+++ user.py
@@ -1,4 +1,6 @@
 import requests
+from .task import Task
+from .habit import Habit
 
 class NewLogin:
 
@@ -21,4 +23,27 @@ class NewLogin:
 			return True
 		else:
 			return False
+
+	def update_habits(self):
+		r = requests.get('https://habitica.com/api/v3/tasks/user', headers=self.credentials, params={'type':'habits'})
+		if r.json()['success']:
+			self.habits = []
+			for task in r.json()['data']:
+				habit = Habit()
+				habit.owner = self
+				habit._id = task['_id']
+				habit.title = task['text']
+				habit.notes = task['notes']
+				habit.tags = task['tags']
+				habit.difficulty = Task.DIFFICULTY[task['priority']]
+				habit.up = task['up']
+				habit.down = task['down']
+				self.habits.append(habit)
+
+	def add_habit(self,title):
+		new_habit = {'text':title,'type':'habit'}
+		r = requests.post('https://habitica.com/api/v3/tasks/user', headers=self.credentials, data=new_habit)
+		self.update_habits()
+
+
 
