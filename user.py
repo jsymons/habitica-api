60ae933939f9b86a25e1f03d3415aa5d53c01cba
Jason Symons <jason.symons@gmail.com>
2016-08-03 07:52:44 -0400

    Retrieve and store user profile

--- user.py
+++ user.py
@@ -4,14 +4,21 @@ class NewLogin:
 
 	def __init__(self):
 		self.login_status = False
-		self.userid = ''
-		self.apiToken = ''
+		self.credentials = {}
 
 	def login(self,username,password):
 		credentials = {'username': username, 'password': password}
 		r = requests.post('https://habitica.com/api/v3/user/auth/local/login', data=credentials)
 		if r.status_code == 200:
-			self.userid = r.json()['data']['id']
-			self.apiToken = r.json()['data']['apiToken']
+			self.credentials['x-api-user'] = r.json()['data']['id']
+			self.credentials['x-api-key'] = r.json()['data']['apiToken']
 			self.login_status = True
+
+	def update_status(self):
+		r = requests.get('https://habitica.com/api/v3/user', headers=self.credentials)
+		if r.json()['success']:
+			self.profile = r.json()['data']
+			return True
+		else:
+			return False
 
